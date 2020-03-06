eaeead7d47edd3604723cd77c299b091def0de05
redspah <redspah@gmail.com>
2020-03-06 03:16:00 +0000

    extreme spaghetti

--- xxhash/test_main.cpp
+++ xxhash/test_main.cpp
@@ -33,12 +33,11 @@ void handler(int sig) {
 #endif
 */
 
-define XXH_STATIC_LINKING_ONLY
+#define XXH_STATIC_LINKING_ONLY
 
 #define XXH_VECTOR 1
 #include "xxhash.hpp"
 
-#
 
 //#include "xxhash.h"
 //#undef XXH_VECTOR
